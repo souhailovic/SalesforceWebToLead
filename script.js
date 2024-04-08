9cf47a00fe541d7334f0fc295aefc7946403e595
souhailovic <souhailkpconsulting@gmail.com>
2024-04-08 16:17:41 +0200

    recuperation date de la demande

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function beforeSubmit() {
-    let datePicker = document.getElementById('date-of-birth');
-    let hiddenDate = document.getElementById('Date_of_Birth__c');
+    let datePicker = document.getElementById('date-de-la-demande');
+    let hiddenDate = document.getElementById('Date_de_la_demande__');
 
     let formattedDate = new Date(datePicker.value).toLocaleDateString("en-US");
     hiddenDate.value = formattedDate;
