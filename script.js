dad79618315da4d8cc298d736e50ac63575abdfe
souhailovic <souhailkpconsulting@gmail.com>
2024-04-08 18:11:14 +0200

    enlever date du form

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function beforeSubmit() {
-    let datePicker = document.getElementById('date-de-la-demande');
-    let hiddenDate = document.getElementById('Date_de_la_demande__c');
+    // let datePicker = document.getElementById('date-de-la-demande');
+    // let hiddenDate = document.getElementById('Date_de_la_demande__c');
 
-    let formattedDate = new Date(datePicker.value).toLocaleDateString("en-US");
-    hiddenDate.value = formattedDate;
+    // let formattedDate = new Date(datePicker.value).toLocaleDateString("en-US");
+    // hiddenDate.value = formattedDate;
 }
