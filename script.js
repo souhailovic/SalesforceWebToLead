766c94364e23c6097ba8a5cd3a1f30869ac184d8
souhailovic <souhailkpconsulting@gmail.com>
2024-04-08 16:28:36 +0200

    correction du script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function beforeSubmit() {
     let datePicker = document.getElementById('date-de-la-demande');
-    let hiddenDate = document.getElementById('Date_de_la_demande__');
+    let hiddenDate = document.getElementById('Date_de_la_demande__c');
 
     let formattedDate = new Date(datePicker.value).toLocaleDateString("en-US");
     hiddenDate.value = formattedDate;
